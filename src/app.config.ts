3b1ba45c44ede0c028daf0969e030df1be3d73bd
rex <rainbowmorel@163.com>
2023-10-17 09:57:09 +0800

    feat: 新增Home页面工具

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,38 +1,39 @@
 export default defineAppConfig({
-  "tabBar": {
-    "color": "#333",
-    "selectedColor": "#3963BC",
-    "backgroundColor": "#FFF",
-    "list": [
+  tabBar: {
+    color: "#333",
+    selectedColor: "#3963BC",
+    backgroundColor: "#FFF",
+    list: [
       {
-        "pagePath": "pages/index/index",
-        "text": "渲染",
-        "iconPath": "assets/images/tabbar/draw.png",
-        "selectedIconPath": "assets/images/tabbar/drawselect.png"
+        pagePath: "pages/index/index",
+        text: "渲染",
+        iconPath: "assets/images/tabbar/draw.png",
+        selectedIconPath: "assets/images/tabbar/drawselect.png",
       },
       {
-        "pagePath": "pages/task/index",
-        "text": "任务列表",
-        "iconPath": "assets/images/tabbar/task.png",
-        "selectedIconPath": "assets/images/tabbar/taskselect.png"
+        pagePath: "pages/task/index",
+        text: "任务列表",
+        iconPath: "assets/images/tabbar/task.png",
+        selectedIconPath: "assets/images/tabbar/taskselect.png",
       },
       {
-        "pagePath": "pages/user/index",
-        "text": "福利",
-        "iconPath": "assets/images/tabbar/task.png",
-        "selectedIconPath": "assets/images/tabbar/taskselect.png"
-      }
-    ]
+        pagePath: "pages/user/index",
+        text: "福利",
+        iconPath: "assets/images/tabbar/task.png",
+        selectedIconPath: "assets/images/tabbar/taskselect.png",
+      },
+    ],
   },
   pages: [
-    'pages/index/index',
-    'pages/task/index',
+    "pages/home/index",
+    "pages/index/index",
+    "pages/task/index",
     "pages/user/index",
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col } from '@nutui/nutui-react-taro'
+
+function Index() {
+
+    return (
+        <>
+            <Row gutter="10">
+                <Col span="12">
+                    艺术二维码
+                </Col>
+
+                <Col span="12">
+                    个性头像
+                </Col>
+
+                <Col span="12">
+                    隐藏文字
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default Index
